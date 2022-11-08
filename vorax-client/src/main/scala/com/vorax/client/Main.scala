c3afaaeb927b2615b821f8e94407d7396ec2988b
benjaminheath238 <benjaminheath238@gmail.com>
2022-11-08 22:08:23 +0000

    first commit

---
+++ vorax-client/src/main/scala/com/vorax/client/Main.scala
@@ -0,0 +1,7 @@
+package com.vorax.client;
+
+object Main {
+    def main(args: Array[String]): Unit = {
+
+    }
+}
---
+++ vorax-client/src/main/scala/com/vorax/client/Script.scala
@@ -0,0 +1,11 @@
+package com.vorax.client
+
+import groovy.lang.{Script => GroovyScript}
+
+class Script extends GroovyScript {
+  def run(): Object = null
+}
+
+object Script {
+    val INIT: int = 0;
+}
