a7fbbbb1f729c535ff01f6d5da0aa35649213e96
benjaminheath238 <benjaminheath238@gmail.com>
2022-11-12 18:57:33 +0000

    Removed old client code

--- vorax-client/src/main/java/com/vorax/client/Main.java
+++
@@ -1,9 +0,0 @@
-package com.vorax.client;
-
-public class Main {
-    public static void main(String[] args) {
-        //Environment env = new Environment(System.getProperty("user.dir"));
-
-        //env.mkdirs("database", "config", "mods");
-    }
-}
